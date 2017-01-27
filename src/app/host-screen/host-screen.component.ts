0429ca4582e500a2dc739fdb77680e7bd30679a5
Eric Parton <ericp@arin.net>
2017-01-27 17:22:24 -0500

    added schema description, fancy button

--- src/app/host-screen/host-screen.component.ts
+++ src/app/host-screen/host-screen.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirebaseObjectObservable, AngularFire} from "angularfire2";
 
 @Component({
-  selector: 'app-host-screen',
-  templateUrl: './host-screen.component.html',
-  styleUrls: ['./host-screen.component.css']
+    selector: 'app-host-screen',
+    templateUrl: './host-screen.component.html',
+    styleUrls: ['./host-screen.component.css']
 })
 export class HostScreenComponent implements OnInit {
 
-  constructor() { }
+    public in_progress: FirebaseObjectObservable<any>;
 
-  ngOnInit() {
-  }
+    constructor(private af: AngularFire) {
+        this.in_progress = af.database.object('/in_progress');
+    }
 
+    ngOnInit() {
+    }
+
+    toggleGame(event: MouseEvent) {
+        this.in_progress.set(event.srcElement.innerHTML.startsWith("Start"));
+    }
 }
